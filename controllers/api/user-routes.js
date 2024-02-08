b57100502424a0bc5cd7ae8176c884f664ed5aa1
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-02-07 20:58:19 -0800

    Add additional models and controllers

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,20 +1,20 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+// const { User } = require("../../models");
 
-//Create new user  
-router.post('/', async (req, res) => {
-    try {
-    const dbuserData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+//Create new user
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
     });
 
     req.session.save(() => {
-      req.session.user_id = dbuserData.id;
+      req.session.user_id = dbUserData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.status(200).json(dbUserData);
     });
   } catch (err) {
     res.status(400).json(err);
@@ -22,18 +22,18 @@ router.post('/', async (req, res) => {
 });
 
 //Login
-router.post('/login', async (req, res) => {
-    try {
+router.post("/login", async (req, res) => {
+  try {
     const dbUserData = await User.findOne({
-         where: { 
-            email: req.body.email 
-        } 
+      where: {
+        email: req.body.email,
+      },
     });
 
     if (!dbUserData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
@@ -42,20 +42,20 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
     req.session.save(() => {
       req.session.logged_in = true;
       console.log(
-        'File: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
+        "File: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie",
         req.session.cookie
       );
 
       res
         .status(200)
-        .json({ user: dbUserData, message: 'You are now logged in!' });
+        .json({ user: dbUserData, message: "You are now logged in!" });
     });
   } catch (err) {
     console.log(err);
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
 });
 
 // Logout
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -73,3 +73,5 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+
+module.exports = router;
