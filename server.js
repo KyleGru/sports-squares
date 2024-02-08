4481c16d02f2a620bc9e0a02dd25c03859edccc6
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-02-07 20:20:05 -0800

    Add controller files

--- server.js
+++ server.js
@@ -1,45 +1,47 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers: require('./utils/helpers') });
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+// Activate when controllers are added.
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = require('express-session');
+const session = require("express-session");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Set up sessions with cookies
 const sess = {
-    secret: process.env.DB_PASSWORD,
-    cookie: {
-      // Stored in milliseconds
-      maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    })
-  };
+  secret: process.env.DB_PASSWORD,
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-  app.use(session(sess));
+// Activate when mySQL is added.
+app.use(session(sess));
 
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-  app.use(routes);
+// Activate when routes is created.
+app.use(routes);
 
-
-  sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, function() {
-        console.log(`App listening on port ${PORT}!`)
-    })
-  })
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, function () {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
