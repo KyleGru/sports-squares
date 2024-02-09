00208a5bd8e375d4dd6721e8ad590a5714c601f1
Zach Smith <zachsmith852@gmail.com>
2024-02-09 14:35:33 -0800

    Add sports api fetch for frontend access

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const sportFetch = require("./sportFetch")
 
 router.use("/users", userRoutes);
+router.use("/sportFetch", sportFetch);
 
 
 
