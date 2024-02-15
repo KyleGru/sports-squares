670ec3a31224922fb29fdb785b0fde4fcbc5a673
Zach Smith <zachsmith852@gmail.com>
2024-02-15 15:22:07 -0800

    Adjust render game logic and set api key

--- controllers/api/gamesAvailable.js
+++ controllers/api/gamesAvailable.js
@@ -16,7 +16,7 @@ router.get('/:sportdate', async (req, res) => {
 
 const dateData = req.params.sportdate
 console.log(dateData);
-  let sportsAPI = `https://replay.sportsdata.io/api/v3/nfl/stats/json/scoresbydate/${dateData}?key=86df436dca9246d888a438b3ac7a19b8`;
+  let sportsAPI = `https://replay.sportsdata.io/api/v3/nfl/stats/json/scoresbydate/${dateData}?key=6c0036a413224a65be46b3c11eccd881`;
   console.log('Date API: ', sportsAPI)
   fetch(sportsAPI).then(function(response) {
     return response.json();
