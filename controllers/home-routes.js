6cab5eeebf959cd3963ea39d8136ba20dec16819
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-02-20 20:13:20 -0800

    Add How To Play TEMP

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,50 +1,49 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, async (req, res) => {
-    try {
+router.get("/", withAuth, async (req, res) => {
+  try {
     const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
     });
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-        users,
-        logged_in: req.session.loggedIn,
+    res.render("homepage", {
+      users,
+      logged_in: req.session.loggedIn,
     });
-
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-    res.redirect('/homepage');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/homepage");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-  console.log('User ID from session', req.session.user_id)
+router.get("/profile", withAuth, async (req, res) => {
+  console.log("User ID from session", req.session.user_id);
   try {
     // Retrieve user data from the database
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
     });
-    console.log(userData)
+    console.log(userData);
     if (!userData) {
-      res.status(404).json({ message: 'User not found' });
+      res.status(404).json({ message: "User not found" });
       return;
     }
-    const serializedUser = userData.get({ plain: true })
-    console.log(serializedUser)
+    const serializedUser = userData.get({ plain: true });
+    console.log(serializedUser);
     // Render the profile page with user data
-    res.render('profile', {
+    res.render("profile", {
       user: serializedUser,
       loggedIn: req.session.loggedIn,
     });
@@ -53,10 +52,19 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-router.get('/homepage', withAuth, (req, res) => {
-  res.render('homepage', {
-    loggedIn: req.session.loggedIn
+router.get("/howToPlay", withAuth, async (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/howToPlay");
+    return;
+  }
+
+  res.render("howToPlay");
+});
+
+router.get("/homepage", withAuth, (req, res) => {
+  res.render("homepage", {
+    loggedIn: req.session.loggedIn,
   });
-})
+});
 
 module.exports = router;
